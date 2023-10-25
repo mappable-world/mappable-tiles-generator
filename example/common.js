ef6b657ba0a3f4a51088da974d211cd59826591d
Maksim Shtyr <sh.maxim97@gmail.com>
2023-10-25 19:06:28 +0300

    fix example

--- example/common.js
+++ example/common.js
@@ -2,21 +2,12 @@
 
 // loader for cartesian projection
 mappable.import.loaders.unshift(async (pkg) => {
-    if (!pkg.includes('@mappable-world/mappable-cartesian-projection')) {
-        return;
-    }
+    if (!pkg.includes('@mappable-world/mappable-cartesian-projection')) return;
 
     await mappable.import.script(`https://unpkg.com/${pkg}/dist/index.js`);
 
     return window['@mappable-world/mappable-cartesian-projection'];
 });
-
-const BOUNDS = [
-    [54.58311, 25.9985],
-    [56.30248, 24.47889]
-];
-
-const LOCATION = {bounds: BOUNDS};
 
 function getEntitiesProps(CartesianProjection, params) {
     const projection = new CartesianProjection(params.projection.bounds);
